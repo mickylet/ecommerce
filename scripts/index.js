1787856ef05fc4f1a53f9eb6d069ed46e2b9d19e
User <windvogelmickylejohnathan@gmail.com>
2021-03-19 09:31:32 +0200

    AND NOW

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,21 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-
-
-
-
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close(){
-    mainMenu.style.top = '-100%';
-}
-
+function addPeople() {
+    const inputs = document.getElementsByTagName("input");
+  
+    fetch("https://guarded-bastion-38164.herokuapp.com/add-new/", {
+      method: "POST",
+      body: JSON.stringify({
+        username: inputs[0].value,
+        password: inputs[1].value,
+        confirm_password: inputs[2].value,
+      }),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        alert("YOU HAVE SUCCESSFULLY REGISTERED");
+        window.location.href = "login.html";
+        document.getElementById("sign-up").reset();
+      });
+  }
